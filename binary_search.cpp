71348bf27530977df0d010892c8fc11fc99bdc23
Zain <amazwings@gmail.com>
2022-10-11 07:07:59 +0500

    Dynamic & Linear S

---
+++ binary_search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int dynamic_search(int a[], int size, int value){
+    int first=0;
+    int last=size-1;
+    bool found=false;
+    int middle;
+    int position=-1;
+    while( first<=last && !found){
+        middle=(first+last)/2;
+        if(a[middle]==value)
+        {
+            found=true;
+            position=middle;
+        }
+        else if(a[middle]>value){
+            last=middle-1;
+        }
+        else
+            first=middle+1;
+
+    }
+    return position;
+}
+int main(){
+    int numbers[20]={01,02,25,35,36,37,44,52,53,65,65,66,67,70,71,73,82,85,98};
+    int result=dynamic_search(numbers, 20, 25);
+    if(result==-1)
+        cout<<"Nothing";
+    else
+        cout<<"Found number at index: "<<result;
+}
---
+++ linear_search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int linear_search(int a[], int size, int value){
+    int s=0;
+    int position=-1;
+    bool found=false;
+    while( s<size && !found){
+        if(a[s]==value)
+        {
+            found=true;
+            position=s;
+        }
+
+    }
+    return position;
+}
+int main(){
+
+    int numbers[20]={01,50,02,73,82,65,52,66,44,85,65,25,35,35,35,98,52,66,44,85};
+    int result=linear_search(numbers, 20, 35);
+  
+    if(result==-1)
+        cout<<"Number wasnt found";
+    else
+        cout<<"Number was found at"<<result;
+
+
+
+}
